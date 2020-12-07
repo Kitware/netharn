4a5ee850da07e21bc8f437a5cb8cb62d5979a422
joncrall <erotemic@gmail.com>
2020-12-07 12:23:48 -0500

    Start branch for 0.5.13

--- super_setup.py
+++ super_setup.py
@@ -804,13 +804,13 @@ DEVEL_REPOS = [
         'remotes': {'public': 'git@gitlab.kitware.com:utils/scriptconfig.git'},
     },
     {
-        'name': 'ndsampler', 'branch': 'dev/0.5.12', 'remote': 'public',
+        'name': 'ndsampler', 'branch': 'dev/0.5.13', 'remote': 'public',
         'remotes': {'public': 'git@gitlab.kitware.com:computer-vision/ndsampler.git'},
     },
 
     # netharn - training harness
     {
-        'name': 'netharn', 'branch': 'dev/0.5.12', 'remote': 'public',
+        'name': 'netharn', 'branch': 'dev/0.5.13', 'remote': 'public',
         'remotes': {'public': 'git@gitlab.kitware.com:computer-vision/netharn.git'},
     },
 ]
