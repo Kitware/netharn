4ec54a67d8a98d7f855960baa8a8b769be602bf5
joncrall <erotemic@gmail.com>
2021-01-26 22:51:06 -0500

    Start branch for 0.5.16

--- super_setup.py
+++ super_setup.py
@@ -768,7 +768,7 @@ def determine_code_dpath():
 DEVEL_REPOS = [
     # The util libs
     {
-        'name': 'kwarray', 'branch': 'dev/0.5.15', 'remote': 'public',
+        'name': 'kwarray', 'branch': 'dev/0.5.16', 'remote': 'public',
         'remotes': {'public': 'git@gitlab.kitware.com:computer-vision/kwarray.git'},
     },
     {
@@ -810,7 +810,7 @@ DEVEL_REPOS = [
 
     # netharn - training harness
     {
-        'name': 'netharn', 'branch': 'dev/0.5.15', 'remote': 'public',
+        'name': 'netharn', 'branch': 'dev/0.5.16', 'remote': 'public',
         'remotes': {'public': 'git@gitlab.kitware.com:computer-vision/netharn.git'},
     },
 ]
