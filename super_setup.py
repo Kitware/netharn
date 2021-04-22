0761824a0b57c0b9afbaafe9f351a76407c8ec13
joncrall <jon.crall@kitware.com>
2021-04-22 14:11:56 -0400

    wip

--- super_setup.py
+++ super_setup.py
@@ -846,7 +846,7 @@ def determine_code_dpath():
 DEVEL_REPOS = [
     # The util libs
     {
-        'name': 'kwarray', 'branch': 'dev/0.5.14', 'remote': 'public',
+        'name': 'kwarray', 'branch': 'dev/0.5.15', 'remote': 'public',
         'remotes': {'public': 'git@gitlab.kitware.com:computer-vision/kwarray.git'},
     },
     {
