89c266a92df8d671ea380993aa18ef5fa791ddc4
joncrall <jon.crall@kitware.com>
2021-05-19 18:05:47 -0400

    wip

--- super_setup.py
+++ super_setup.py
@@ -883,7 +883,7 @@ DEVEL_REPOS = [
         'remotes': {'public': 'git@gitlab.kitware.com:utils/scriptconfig.git'},
     },
     {
-        'name': 'ndsampler', 'branch': 'dev/0.6.2', 'remote': 'public',
+        'name': 'ndsampler', 'branch': 'dev/0.6.3', 'remote': 'public',
         'remotes': {'public': 'git@gitlab.kitware.com:computer-vision/ndsampler.git'},
     },
 
