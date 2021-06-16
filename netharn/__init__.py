4491b6e99a40a509556024df39dd192b8f184191
joncrall <jon.crall@kitware.com>
2021-06-16 11:39:23 -0400

    Start branch for 0.5.17

--- netharn/__init__.py
+++ netharn/__init__.py
@@ -4,7 +4,7 @@
 mkinit netharn --noattrs --dry
 mkinit netharn --noattrs
 """
-__version__ = '0.5.16'
+__version__ = '0.5.17'
 
 try:
     # PIL 7.0.0 removed PIL_VERSION, which breaks torchvision, monkey patch it
