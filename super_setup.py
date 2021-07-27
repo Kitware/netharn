7b50b0e326f469d34f8d0d469b1c8f0093a7be11
joncrall <jon.crall@kitware.com>
2021-07-27 19:56:40 -0400

    wip

--- super_setup.py
+++ super_setup.py
@@ -850,7 +850,7 @@ DEVEL_REPOS = [
         'remotes': {'public': 'git@gitlab.kitware.com:computer-vision/kwarray.git'},
     },
     {
-        'name': 'kwimage', 'branch': 'dev/0.7.9', 'remote': 'public',
+        'name': 'kwimage', 'branch': 'dev/0.7.10', 'remote': 'public',
         'remotes': {'public': 'git@gitlab.kitware.com:computer-vision/kwimage.git'},
     },
     # TODO:
