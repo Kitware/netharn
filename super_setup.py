a583bd3f239994d518c1341a8d1f9859f7b4c5b0
joncrall <jon.crall@kitware.com>
2021-12-08 13:56:04 -0500

    wip

--- super_setup.py
+++ super_setup.py
@@ -850,7 +850,7 @@ DEVEL_REPOS = [
         'remotes': {'origin': 'git@gitlab.kitware.com:computer-vision/kwarray.git'},
     },
     {
-        'name': 'kwimage', 'branch': 'dev/0.7.15', 'remote': 'origin',
+        'name': 'kwimage', 'branch': 'dev/0.7.17', 'remote': 'origin',
         'remotes': {'origin': 'git@gitlab.kitware.com:computer-vision/kwimage.git'},
     },
     # TODO:
@@ -859,7 +859,7 @@ DEVEL_REPOS = [
     #     'remotes': {'origin': 'git@gitlab.kitware.com:computer-vision/kwannot.git'},
     # },
     {
-        'name': 'kwcoco', 'branch': 'dev/0.2.18', 'remote': 'origin',
+        'name': 'kwcoco', 'branch': 'dev/0.2.19', 'remote': 'origin',
         'remotes': {'origin': 'git@gitlab.kitware.com:computer-vision/kwcoco.git'},
     },
     {
